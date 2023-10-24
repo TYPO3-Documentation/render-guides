9ed8f520eeabf00c26ad3f90f546a7fc685cf121
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2023-10-24 17:09:37 +0200

    [FEATURE] Introduce PHP stan (#11)
    Put existings errors in the baseline for now, they can be analyzed and
    solved in a follow up

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,6 +2,7 @@
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config
+    ->setCacheFile('.cache/.php-cs-fixer.cache')
     ->getFinder()->in(__DIR__)
     ->exclude([
         'docs',
