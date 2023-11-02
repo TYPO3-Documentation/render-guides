bfbe06636763630fbbec67b424a2543f412bfdf4
Jaap van Otterdijk <jaap@ijaap.nl>
2023-11-02 10:40:22 +0100

    [FEATURE] Add automated input detection (#20)
    By decorating the run command we can add extra functionality to the
    default command of guides. In this case I added the input guessing logic
    to find the correct input format or single file readme documentation.

---
+++ packages/typo3-guides-extension/resources/config/typo3-guides.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use phpDocumentor\Guides\Cli\Command\Run;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+use T3Docs\GuidesExtension\Command\RunDecorator;
+
+return static function (ContainerConfigurator $container): void {
+    $container->services()
+        ->defaults()
+        ->autowire()
+        ->set(RunDecorator::class)
+        ->decorate(
+            Run::class,
+        )->args([service('.inner')]);
+};
---
+++ packages/typo3-guides-extension/src/DependencyInjection/Typo3GuidesExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace T3Docs\GuidesExtension\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+final class Typo3GuidesExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new PhpFileLoader(
+            $container,
+            new FileLocator(dirname(__DIR__, 2) . '/resources/config'),
+        );
+
+        $loader->load('typo3-guides.php');
+    }
+}
