db2e36e0ebcff9c8994a55adf8eaca7cede9105b
lina.wolf <lwolf@w-commerce.de>
2023-11-05 06:11:52 +0100

    [FEATURE] Implement t3-field-list-table
    Depends on #40

--- packages/typo3-docs-theme/src/Directives/T3FieldListTableDirective.php
+++ packages/typo3-docs-theme/src/Directives/T3FieldListTableDirective.php
@@ -14,13 +14,28 @@ declare(strict_types=1);
 namespace T3Docs\Typo3DocsTheme\Directives;
 
 use phpDocumentor\Guides\Nodes\CollectionNode;
+use phpDocumentor\Guides\Nodes\FieldListNode;
+use phpDocumentor\Guides\Nodes\ListNode;
 use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\Nodes\Table\TableColumn;
+use phpDocumentor\Guides\Nodes\Table\TableRow;
+use phpDocumentor\Guides\Nodes\TableNode;
 use phpDocumentor\Guides\RestructuredText\Directives\SubDirective;
 use phpDocumentor\Guides\RestructuredText\Parser\BlockContext;
 use phpDocumentor\Guides\RestructuredText\Parser\Directive;
+use phpDocumentor\Guides\RestructuredText\Parser\Productions\Rule;
+use Psr\Log\LoggerInterface;
 
 class T3FieldListTableDirective extends SubDirective
 {
+    /** @param Rule<CollectionNode> $startingRule */
+    public function __construct(
+        protected Rule $startingRule,
+        protected LoggerInterface $logger,
+    ) {
+        parent::__construct($startingRule);
+    }
+
     public function getName(): string
     {
         return 't3-field-list-table';
@@ -31,6 +46,47 @@ class T3FieldListTableDirective extends SubDirective
         CollectionNode $collectionNode,
         Directive $directive,
     ): Node|null {
-        return $collectionNode;
+        $i = 0;
+        $headers = [];
+        $rows = [];
+        foreach ($collectionNode->getChildren() as $list) {
+            if (!$list instanceof ListNode) {
+                $this->logger->warning(sprintf('Only lists are allowed in a t3-field list. Node of type %s found.', $list::class), $blockContext->getLoggerInformation());
+                continue;
+            }
+            foreach ($list->getChildren() as $listItem) {
+                if ($i === 0) {
+                    $header = new TableRow();
+                    foreach ($listItem->getChildren() as $fieldlist) {
+                        if (!$fieldlist instanceof FieldListNode) {
+                            $this->logger->warning(sprintf('Only field lists are allowed in each list item a t3-field list. Node of type %s found.', $list::class), $blockContext->getLoggerInformation());
+                            continue;
+                        }
+                        foreach ($fieldlist->getChildren() as $fieldlistItem) {
+                            $columnNode = new TableColumn($fieldlistItem->getTerm(), 1, $fieldlistItem->getChildren());
+                            $header->addColumn($columnNode);
+                        }
+                    }
+                    $headers[] = $header;
+                    $i++;
+                    continue;
+                }
+                $row = new TableRow();
+                foreach ($listItem->getChildren() as $fieldlist) {
+                    if (!$fieldlist instanceof FieldListNode) {
+                        $this->logger->warning(sprintf('Only field lists are allowed in each list item a t3-field list. Node of type %s found.', $list::class), $blockContext->getLoggerInformation());
+                        continue;
+                    }
+                    foreach ($fieldlist->getChildren() as $fieldlistItem) {
+                        $columnNode = new TableColumn($fieldlistItem->getTerm(), 1, $fieldlistItem->getChildren());
+                        $row->addColumn($columnNode);
+                    }
+                }
+                $rows[] = $row;
+                $i++;
+            }
+        }
+        $tableNode = new TableNode($rows, $headers);
+        return $tableNode;
     }
 }
---
+++ tests/Integration/tests/table/t3-field-list-table/input/index.rst
@@ -0,0 +1,15 @@
+==========================
+t3-field-list-table tables
+==========================
+
+..  t3-field-list-table::
+    :header-rows: 1
+
+    -   :Header1:   Header1
+        :Header2:   Header2
+
+    -   :Header1:   1
+        :Header2:   one
+
+    -   :Header1:   2
+        :Header2:   two
