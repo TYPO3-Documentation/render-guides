5773cb42619df7d1a2f2d876c285fe7ea6317cc3
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-11-14 19:12:54 +0100

    Fix cgl

--- packages/php-domain/resources/config/php-domain.php
+++ packages/php-domain/resources/config/php-domain.php
@@ -7,8 +7,9 @@ use phpDocumentor\Guides\RestructuredText\Directives\SubDirective;
 use phpDocumentor\Guides\RestructuredText\Parser\Productions\DirectiveContentRule;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-use T3Docs\PhpDomain\Directives\Php\EnumDirective;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+use T3Docs\PhpDomain\Directives\Php\EnumDirective;
 
 use T3Docs\PhpDomain\Directives\Php\InterfaceDirective;
 use T3Docs\PhpDomain\Directives\Php\MethodDirective;
