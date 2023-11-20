8347240e5aed6a184773f44c707b59ff33314e19
Garvin Hicking <blog@garv.in>
2023-11-20 11:45:25 +0100

    [DOCS] Improve error output on entrypoint.sh when running on macOS

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,14 @@ if [ "$(id -u)" -eq "0" ]; then
   GID=$(stat -c "%g" $(pwd))
 
   if [ "$UID" -eq "0" ]; then
-        echo "Could not detect the owner of $(pwd) did you mount your project?."
-        echo "Run this container with \"docker run --rm  --volume \${PWD}:/project ghcr.io/typo3-documentation/render-guides:main\""
+        echo "ERROR: Could not detect the owner of $(pwd) - did you mount your project?"
+        echo ""
+        echo "Run this container with:"
+        echo "\"docker run --rm --volume \${PWD}:/project ghcr.io/typo3-documentation/render-guides:main\""
+        echo ""
+        echo "On macOS you might need to specify the user:"
+        echo "\"docker run --rm --volume --user=\$(id -u):\$(id -g) \${PWD}:/project ghcr.io/typo3-documentation/render-guides:main\""
+        echo ""
         exit 1
   else
       addgroup typo3 --gid=$GID;
