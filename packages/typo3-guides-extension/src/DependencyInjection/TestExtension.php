671e0f1cbad9febecdcc760f9adcd81fd5386f54
lina.wolf <lwolf@w-commerce.de>
2023-11-20 18:29:24 +0100

    [FEATURE] Output Last rendered date
    And mock it in the integration tests so that integration tests are still possible

---
+++ packages/typo3-guides-extension/src/DependencyInjection/TestExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace T3Docs\GuidesExtension\DependencyInjection;
+
+use Monolog\Handler\TestHandler;
+use Monolog\Logger;
+use phpDocumentor\Guides\Compiler\Compiler;
+use phpDocumentor\Guides\Parser;
+use Psr\Clock\ClockInterface;
+use Symfony\Component\Clock\MockClock;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TestExtension extends Extension implements CompilerPassInterface
+{
+    /** @param array<mixed> $configs */
+    public function load(array $configs, ContainerBuilder $container): void {}
+
+    public function process(ContainerBuilder $container): void
+    {
+        $container->getDefinition(Parser::class)->setPublic(true);
+        $container->getDefinition(Compiler::class)->setPublic(true);
+        $container->getDefinition('phpdoc.guides.output_node_renderer')->setPublic(true);
+
+        $clockDefinition = new Definition(MockClock::class, ['2023-01-01 12:00:00']);
+        $container->setDefinition(ClockInterface::class, $clockDefinition);
+
+        $container->register(TestHandler::class, TestHandler::class)->setPublic(true);
+        $container->getDefinition(Logger::class)
+            ->addMethodCall('pushHandler', [new Reference(TestHandler::class)]);
+    }
+}
--- tests/ApplicationTestCase.php
+++ tests/ApplicationTestCase.php
@@ -7,11 +7,11 @@ namespace T3Docs\Typo3DocsTheme;
 use phpDocumentor\Guides\Bootstrap\DependencyInjection\BootstrapExtension;
 use phpDocumentor\Guides\Cli\DependencyInjection\ApplicationExtension;
 use phpDocumentor\Guides\Cli\DependencyInjection\ContainerFactory;
-use phpDocumentor\Guides\DependencyInjection\TestExtension;
 use phpDocumentor\Guides\Markdown\DependencyInjection\MarkdownExtension;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use T3Docs\GuidesExtension\DependencyInjection\TestExtension;
 use T3Docs\PhpDomain\DependencyInjection\PhpDomainExtension;
 use T3Docs\Typo3DocsTheme\DependencyInjection\Typo3DocsThemeExtension;
 
