dddbcc9ebb622d68aeff48708c960c3cc1aa4a8c
Garvin Hicking <blog@garv.in>
2023-12-07 09:03:58 +0100

    [TASK] Add debugging to entrypoint.sh
    (also updates composer dependencies to current)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,18 @@
 # TODO: This detection seems to fail on macOS. If no "--user" argument is specified to make Docker run
 #       as that user, it cannot deduce ownership properly. Probably the Dockerfile needs to be adapted
 #       so that a user-switch to the "typo3" user is done? This can have side-effects though.
-if [ "$(id -u)" -eq "0" ]; then
+MY_UID="$(id -u)"
+echo "UID: $MY_UID"
+
+if [ "$MY_UID" -eq "0" ]; then
 
   UID=$(stat -c "%u" $(pwd))
   GID=$(stat -c "%g" $(pwd))
 
   if [ "$UID" -eq "0" ]; then
-        echo "ERROR: Could not detect the owner of $(pwd) - did you mount your project?"
-        echo ""
-        echo "Run this container with:"
-        echo "\"docker run --rm --volume \${PWD}:/project ghcr.io/typo3-documentation/render-guides:main\""
-        echo ""
-        echo "On macOS you might need to specify the user:"
-        echo "\"docker run --rm --volume --user=\$(id -u):\$(id -g) \${PWD}:/project ghcr.io/typo3-documentation/render-guides:main\""
-        echo ""
-        exit 1
+      echo "Run-as: root"
   else
+      echo "Run-as: $UID (custom invocation)"
       addgroup typo3 --gid=$GID;
       adduser -h $(pwd) -D -G typo3 --uid=$UID typo3;
 
@@ -32,8 +28,13 @@ if [ "$(id -u)" -eq "0" ]; then
       done
       chmod a+x /usr/local/bin/invocation.sh
 
+      echo "Invocation:"
+      cat /usr/local/bin/invocation.sh
+      echo ""
       su - typo3 -c "/usr/local/bin/invocation.sh"
   fi
 else
+  echo "Run-as: Owner $MY_UID"
+  echo "Invocation: php $@"
   php "$@"
 fi
