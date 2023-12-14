eee0a04a66c59e3188b6488ce611567c8cc1f596
lina.wolf <lwolf@w-commerce.de>
2023-12-14 19:24:45 +0100

    [TASK] Use Bootstrap tabs instead of homemade tabs

--- packages/typo3-docs-theme/src/Directives/GroupTabDirective.php
+++ packages/typo3-docs-theme/src/Directives/GroupTabDirective.php
@@ -16,13 +16,25 @@ namespace T3Docs\Typo3DocsTheme\Directives;
 use phpDocumentor\Guides\Nodes\CollectionNode;
 use phpDocumentor\Guides\Nodes\InlineCompoundNode;
 use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\ReferenceResolvers\AnchorReducer;
 use phpDocumentor\Guides\RestructuredText\Directives\SubDirective;
 use phpDocumentor\Guides\RestructuredText\Parser\BlockContext;
 use phpDocumentor\Guides\RestructuredText\Parser\Directive;
+use phpDocumentor\Guides\RestructuredText\Parser\Productions\Rule;
+use phpDocumentor\Guides\RestructuredText\TextRoles\GenericLinkProvider;
+use Psr\Log\LoggerInterface;
+use T3Docs\GuidesPhpDomain\PhpDomain\FullyQualifiedNameService;
+use T3Docs\GuidesPhpDomain\PhpDomain\ModifierService;
 use T3Docs\Typo3DocsTheme\Nodes\GroupTabNode;
 
 class GroupTabDirective extends SubDirective
 {
+    public function __construct(
+        Rule $startingRule,
+        private readonly AnchorReducer $anchorReducer,
+    ) {
+        parent::__construct($startingRule);
+    }
     public function getName(): string
     {
         return 'group-tab';
@@ -33,11 +45,13 @@ class GroupTabDirective extends SubDirective
         CollectionNode $collectionNode,
         Directive $directive,
     ): Node|null {
+        $key = $this->anchorReducer->reduceAnchor($directive->getData());
         return new GroupTabNode(
             'group-tab',
             $directive->getData(),
             $directive->getDataNode() ?? new InlineCompoundNode(),
-            $collectionNode->getChildren()
+            $key,
+            $collectionNode->getChildren(),
         );
     }
 }
