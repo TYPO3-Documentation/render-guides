fc907ef6a03b4ac4570ae57132855c403b48e389
lina.wolf <lwolf@w-commerce.de>
2023-12-29 17:14:15 +0100

    [TASK] Remove hack from menu javascript
    This hack was used to hide the 3000 entry long list of exceptions in the Exception list.
    
    with the newly introduced setting 'max-menu-depth="1"' we can limit the menu in that manual (which also greatly improves performance) and get rid of this hack in JavaScript

--- packages/typo3-docs-theme/assets/js/menu-expandable.js
+++ packages/typo3-docs-theme/assets/js/menu-expandable.js
@@ -38,14 +38,3 @@
 
   makeMenuExpandable();
 })();
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const currentFirstLevelEntries = document.querySelectorAll('.menu-level-1 > li.active');
-
-  Array.from(currentFirstLevelEntries).filter(entry => {
-    if (entry.textContent.trim().startsWith('TYPO3 Exceptions')) {
-      entry.classList.remove('active');
-    }
-  });
-});
