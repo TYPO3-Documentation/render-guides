39838b8e7fe64f2abc889cccd81343c22e65f181
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-12-30 11:45:23 +0100

    [TASK] Give feedback on copying code block
    Before, when you copy a code block via the copy button no feedback is given.
    Now, a green check mark is displayed with a tooltip "Copied!" - which are
    removed again after 3 seconds. This is the GitHub way displaying copied snippets.
    
    Additionally, Font Awesome icons are now used.

--- packages/typo3-docs-theme/assets/js/code-clipboard.js
+++ packages/typo3-docs-theme/assets/js/code-clipboard.js
@@ -1,21 +1,43 @@
 (() => {
   const SELECTOR_BUTTON = '.code-block-copy';
   const SELECTOR_CODE = '.code-block';
+  const SELECTOR_HIDE_CLASS = 'code-block-hide';
+  const SELECTOR_ICON_COPY = '.code-block-copy-icon';
+  const SELECTOR_ICON_CHECK = '.code-block-check-icon';
   const SELECTOR_PARENT = '.code-block-wrapper';
+  const SELECTOR_TOOLTIP_CHECK = '.code-block-check-tooltip';
+
+  const COPIED_TIMEOUT_MILLISECONDS = 3000;
 
   if (!navigator.clipboard && !navigator.clipboard.writeText) {
     console.info('"navigator.clipboard.writeText" is not available. Update to a modern browser to copy code to the system\'s clipboard');
     return;
   }
 
+  const toggleCopyClasses = (wrapperElement) => {
+    const copyIconElement = wrapperElement.querySelector(SELECTOR_ICON_COPY);
+    const checkIconElement = wrapperElement.querySelector(SELECTOR_ICON_CHECK);
+    const tooltipElement = wrapperElement.querySelector(SELECTOR_TOOLTIP_CHECK);
+
+    copyIconElement.classList.toggle(SELECTOR_HIDE_CLASS);
+    checkIconElement.classList.toggle(SELECTOR_HIDE_CLASS);
+    tooltipElement.classList.toggle(SELECTOR_HIDE_CLASS);
+  };
+
   [...document.querySelectorAll(SELECTOR_BUTTON)].forEach(item => {
     item.addEventListener('click', event => {
-      const codeBlockElement = event.target.closest(SELECTOR_PARENT).querySelector(SELECTOR_CODE);
+      const wrapperElement = event.target.closest(SELECTOR_PARENT);
+      const codeBlockElement = wrapperElement.querySelector(SELECTOR_CODE);
       if (!codeBlockElement) {
         console.warn('Cannot copy code as no code block is available!');
         return;
       }
       navigator.clipboard.writeText(codeBlockElement.textContent);
+
+      toggleCopyClasses(wrapperElement);
+      setTimeout(() => {
+        toggleCopyClasses(wrapperElement);
+      }, COPIED_TIMEOUT_MILLISECONDS);
     });
   });
 })();
