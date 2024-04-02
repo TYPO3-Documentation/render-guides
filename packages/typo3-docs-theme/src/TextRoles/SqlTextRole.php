ebeae15d33a11b5689890fa48b8fb4e5f2c6578a
Garvin Hicking <blog@garv.in>
2024-04-02 13:08:48 +0200

    [TASK] Add SQL popover highlighting

---
+++ packages/typo3-docs-theme/src/TextRoles/SqlTextRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace T3Docs\Typo3DocsTheme\TextRoles;
+
+use phpDocumentor\Guides\Nodes\Inline\InlineNode;
+use phpDocumentor\Guides\RestructuredText\Parser\DocumentParserContext;
+use phpDocumentor\Guides\RestructuredText\TextRoles\TextRole;
+use T3Docs\Typo3DocsTheme\Nodes\Inline\CodeInlineNode;
+
+final class SqlTextRole implements TextRole
+{
+    public function getName(): string
+    {
+        return 'sql';
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    public function processNode(DocumentParserContext $documentParserContext, string $role, string $content, string $rawContent): InlineNode
+    {
+        return new CodeInlineNode($rawContent, 'SQL', 'Structured Query Language for database queries.');
+    }
+}
