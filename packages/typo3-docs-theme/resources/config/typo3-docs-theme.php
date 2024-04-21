9e7d18c2e03e5e7705211257c2ee11e2b5c084b1
lina.wolf <lwolf@w-commerce.de>
2024-04-21 14:03:54 +0200

    [TASK] Exclude Sitemap from search
    Additionally unskip the sitemap test as it is working again.

--- packages/typo3-docs-theme/resources/config/typo3-docs-theme.php
+++ packages/typo3-docs-theme/resources/config/typo3-docs-theme.php
@@ -26,6 +26,7 @@ use T3Docs\Typo3DocsTheme\EventListeners\IgnoreLocalizationsFolders;
 use T3Docs\Typo3DocsTheme\EventListeners\TestingModeActivator;
 use T3Docs\Typo3DocsTheme\Inventory\Typo3InventoryRepository;
 use T3Docs\Typo3DocsTheme\Parser\ExtendedInterlinkParser;
+use T3Docs\Typo3DocsTheme\Parser\Productions\FieldList\TemplateFieldListItemRule;
 use T3Docs\Typo3DocsTheme\Renderer\DecoratingPlantumlRenderer;
 use T3Docs\Typo3DocsTheme\TextRoles\FluidTextTextRole;
 use T3Docs\Typo3DocsTheme\TextRoles\HtmlTextTextRole;
@@ -93,6 +94,9 @@ return static function (ContainerConfigurator $container): void {
         ->tag('phpdoc.guides.parser.rst.text_role')
         ->set(YamlTextTextRole::class)
         ->tag('phpdoc.guides.parser.rst.text_role')
+
+        ->set(TemplateFieldListItemRule::class)
+        ->tag('phpdoc.guides.parser.rst.fieldlist')
 
 
         ->set(DecoratingPlantumlRenderer::class)
