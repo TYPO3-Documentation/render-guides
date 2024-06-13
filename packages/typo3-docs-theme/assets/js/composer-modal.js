257ffa0ac105f911e30b1b35238f81ba6c355108
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-06-13 19:06:06 +0200

    [TASK] Display Source and Issue links for composer role (#612)
    ![image](https://github.com/TYPO3-Documentation/render-guides/assets/48202465/aad09555-4ebd-4c3f-96d5-4901cf50009d)

--- packages/typo3-docs-theme/assets/js/composer-modal.js
+++ packages/typo3-docs-theme/assets/js/composer-modal.js
@@ -50,6 +50,24 @@
             <button type="button" class="btn btn-outline-secondary copy-button" data-target="composer-command"><i class="far fa-clone"></i></button>
         </div>
   `;
+    var links = '';
+    if (item.dataset.source){
+      const url = new URL(item.dataset.source);
+      var srcString = 'Source';
+      if (url.hostname === 'github.com') {
+        srcString = 'GitHub';
+      }
+      if (url.hostname === 'gitlab.com') {
+        srcString = 'GitLab';
+      }
+      links += `<a class="btn btn-light" href="${item.dataset.source}">${srcString}</a>`;
+    }
+    if (item.dataset.issues){
+      links += `<a class="btn btn-light" href="${item.dataset.issues}">Report issue</a>`;
+    }
+    if(links) {
+      content.innerHTML += `<div class="btn-group mt-2" role="group" aria-label="Links to GitHub / GitLab">${links}</div>`;
+    }
     const generalModalCustomButtons = generalModal.querySelector('#generalModalCustomButtons');
 
     // Add more buttons to the modal footer
