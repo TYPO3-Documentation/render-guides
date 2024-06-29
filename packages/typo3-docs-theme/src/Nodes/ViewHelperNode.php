cbfd4d2284965cdbb2501f75e2717775b008bf20
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-06-29 09:33:53 +0200

    [FEATURE] Introduce viewhelper directive (#622)
    This directive displays the ViewHelper from a json input

---
+++ packages/typo3-docs-theme/src/Nodes/ViewHelperNode.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace T3Docs\Typo3DocsTheme\Nodes;
+
+use phpDocumentor\Guides\Nodes\Inline\PlainTextInlineNode;
+use phpDocumentor\Guides\Nodes\InlineCompoundNode;
+use phpDocumentor\Guides\Nodes\LinkTargetNode;
+use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\Nodes\OptionalLinkTargetsNode;
+use phpDocumentor\Guides\Nodes\PrefixedLinkTargetNode;
+use phpDocumentor\Guides\RestructuredText\Nodes\GeneralDirectiveNode;
+
+final class ViewHelperNode extends GeneralDirectiveNode implements LinkTargetNode, OptionalLinkTargetsNode, PrefixedLinkTargetNode
+{
+    public const LINK_TYPE = 'typo3:viewhelper';
+    public const LINK_PREFIX = 'viewhelper-';
+    /**
+     * @param Node[] $documentation
+     * @param array<string, string> $docTags
+     * @param array<string, ViewHelperArgumentNode> $arguments
+     */
+    public function __construct(
+        private readonly string $id,
+        private readonly string $tagName,
+        private readonly string $shortClassName,
+        private readonly string $namespace,
+        private readonly string $className,
+        private readonly array $documentation,
+        private readonly string $xmlNamespace,
+        private readonly bool $allowsArbitraryArguments,
+        private readonly array $docTags,
+        private readonly string $gitHubLink = '',
+        private readonly bool $noindex = false,
+        private array $arguments = [],
+    ) {
+        parent::__construct('viewhelper', $tagName, new InlineCompoundNode([new PlainTextInlineNode($tagName)]), $documentation);
+    }
+
+    public function getTagName(): string
+    {
+        return $this->tagName;
+    }
+
+    public function getShortClassName(): string
+    {
+        return $this->shortClassName;
+    }
+
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
+    public function getClassName(): string
+    {
+        return $this->className;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getDocumentation(): array
+    {
+        return $this->documentation;
+    }
+
+    public function getXmlNamespace(): string
+    {
+        return $this->xmlNamespace;
+    }
+
+    public function isAllowsArbitraryArguments(): bool
+    {
+        return $this->allowsArbitraryArguments;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDocTags(): array
+    {
+        return $this->docTags;
+    }
+
+    /**
+     * @return ViewHelperArgumentNode[]
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @param ViewHelperArgumentNode[] $arguments
+     */
+    public function setArguments(array $arguments): void
+    {
+        $this->arguments = $arguments;
+    }
+
+    public function getLinkType(): string
+    {
+        return self::LINK_TYPE;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getLinkText(): string
+    {
+        return $this->tagName;
+    }
+
+    public function getAnchor(): string
+    {
+        return self::LINK_PREFIX . $this->id;
+    }
+
+    public function isNoindex(): bool
+    {
+        return $this->noindex;
+    }
+
+    public function getPrefix(): string
+    {
+        return self::LINK_PREFIX;
+    }
+
+    public function getGitHubLink(): string
+    {
+        return $this->gitHubLink;
+    }
+}
