d4b3d68075591c566e43c4bb2149d9d8ffed2b6b
lina.wolf <lwolf@w-commerce.de>
2024-09-22 10:22:12 +0200

    [FEATURE] Render main menu JSON

--- packages/typo3-guides-extension/resources/config/typo3-guides.php
+++ packages/typo3-guides-extension/resources/config/typo3-guides.php
@@ -7,9 +7,9 @@ use phpDocumentor\Guides\Interlink\InventoryRepository;
 use phpDocumentor\Guides\Renderer\UrlGenerator\UrlGeneratorInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use T3Docs\GuidesExtension\Command\RunDecorator;
-use T3Docs\Typo3DocsTheme\Inventory\Typo3InventoryRepository;
 use T3Docs\GuidesExtension\Renderer\UrlGenerator\RenderOutputUrlGenerator;
 use T3Docs\GuidesExtension\Renderer\UrlGenerator\SingleHtmlUrlGenerator;
+use T3Docs\Typo3DocsTheme\Inventory\Typo3InventoryRepository;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
