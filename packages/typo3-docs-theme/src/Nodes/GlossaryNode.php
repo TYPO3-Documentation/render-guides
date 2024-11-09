1a00250078ac58069602e8a26b667fa852028ec5
lina.wolf <lwolf@w-commerce.de>
2024-11-09 11:59:08 +0100

    [FEATURE] Introduce an automatically sorted Glossary

---
+++ packages/typo3-docs-theme/src/Nodes/GlossaryNode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace T3Docs\Typo3DocsTheme\Nodes;
+
+use phpDocumentor\Guides\Nodes\InlineCompoundNode;
+use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\RestructuredText\Nodes\GeneralDirectiveNode;
+
+final class GlossaryNode extends GeneralDirectiveNode
+{
+    /**
+     * @param Node[] $value
+     * @param array<string, array<string, Node>> $entries
+     */
+    public function __construct(
+        protected readonly string $plainContent,
+        protected readonly InlineCompoundNode $content,
+        array $value = [],
+        protected array $entries = [],
+    ) {
+        parent::__construct('glossary', $plainContent, $content, $value);
+    }
+
+    /**
+     * @return array<string, array<string, Node>>
+     */
+    public function getEntries(): array
+    {
+        return $this->entries;
+    }
+}
