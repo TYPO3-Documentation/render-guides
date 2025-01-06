ed6db1bfb8dec39bc8231fef4247436c4a13d5c3
lina.wolf <lwolf@w-commerce.de>
2025-01-06 14:18:33 +0100

    [BUGFIX] Replace typo3.azureedge.net with cdn.typo3.com

--- packages/typo3-docs-theme/src/Twig/TwigExtension.php
+++ packages/typo3-docs-theme/src/Twig/TwigExtension.php
@@ -71,7 +71,7 @@ final class TwigExtension extends AbstractExtension
             // executions, and sets links to resources/assets to a public CDN.
             // Outside CI (and for local development) all Assets are linked locally.
             // This is prevented when being run within PHPUnit.
-            $this->typo3AzureEdgeURI = 'https://typo3.azureedge.net/typo3documentation/theme/typo3-docs-theme/' . getenv('TYPO3AZUREEDGEURIVERSION') . '/';
+            $this->typo3AzureEdgeURI = 'https://cdn.typo3.com/typo3documentation/theme/typo3-docs-theme/' . getenv('TYPO3AZUREEDGEURIVERSION') . '/';
         }
     }
 
