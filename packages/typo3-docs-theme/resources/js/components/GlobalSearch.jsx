e5d6df673ff540e78127920dd2b988fca6423053
Colin Regourd <cregourd@premieroctet.com>
2025-03-25 18:12:51 +0100

    Fix integrity test

--- packages/typo3-docs-theme/resources/js/components/GlobalSearch.jsx
+++ packages/typo3-docs-theme/resources/js/components/GlobalSearch.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import SearchModal from './SearchModal';
 
-const GlobalSearch = () => {
+const GlobalSearch = ({ displayInput = false }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() => {
+        const url = new URL(window.location.href);
+        const queryParam = url.searchParams.get('q');
+        if (queryParam) {
+            setSearchQuery(queryParam);
+        }
+    }, []);
 
     const handleButtonClick = (e) => {
         e.preventDefault();
@@ -22,7 +31,11 @@ const GlobalSearch = () => {
                 isOpen={isModalOpen}
                 onClose={() => setIsModalOpen(false)}
             />}
-            <button onClick={handleButtonClick} class="btn btn-light"><i class="fa fa-search"></i>&nbsp;<span class="d-none d-md-inline">Search</span></button>
+            {displayInput ? <div class="input-group mb-3 mt-sm-3" onClick={handleButtonClick}>
+                <input autocomplete="off" class="form-control shadow-none" id="globalsearchinput" name="q" placeholder="TYPO3 documentation..." type="text" value={searchQuery}></input>
+                <button class="btn btn-light"><i class="fa fa-search"></i>&nbsp;<span class="d-none d-md-inline">Search</span></button>
+            </div> :
+                <button onClick={handleButtonClick} class="btn btn-light"><i class="fa fa-search"></i>&nbsp;<span class="d-none d-md-inline">Search</span></button>}
         </>
     );
 };
--- packages/typo3-docs-theme/resources/js/search.jsx
+++ packages/typo3-docs-theme/resources/js/search.jsx
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('global-search-root');
     if (container) {
         const root = createRoot(container);
-        root.render(<GlobalSearch />);
+        root.render(<GlobalSearch/>);
+    }
+
+    const resultsContainer = document.getElementById('global-search-results');
+    if (resultsContainer) {
+        const root = createRoot(resultsContainer);
+        root.render(<GlobalSearch displayInput={true} />);
     }
 });
