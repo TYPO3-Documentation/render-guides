24c1dbb61a180fac483c599e4f17f0cda685f459
Colin Regourd <cregourd@premieroctet.com>
2025-03-25 17:09:05 +0100

    Fix mobile search block position, allow search in current and remove select, hide form

--- packages/typo3-docs-theme/resources/js/components/GlobalSearch.jsx
+++ packages/typo3-docs-theme/resources/js/components/GlobalSearch.jsx
@@ -4,41 +4,16 @@ import SearchModal from './SearchModal';
 const GlobalSearch = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleInputClick = () => {
-        setIsModalOpen(true);
-    };
-
     const handleButtonClick = (e) => {
         e.preventDefault();
         setIsModalOpen(true);
     };
 
     useEffect(() => {
-        let globalSearchInput = document.getElementById('globalsearchinput');
-        let globalSearchButton = document.querySelector('#global-search-form button');
-
-        if (!globalSearchInput) {
-            globalSearchInput = document.getElementById('searchinput');
+        const form = document.getElementById('global-search-form');
+        if (form) {
+            form.hidden = true;
         }
-
-        if (globalSearchInput) {
-            globalSearchInput.addEventListener('click', handleInputClick);
-        }
-
-        if (globalSearchButton) {
-            globalSearchButton.addEventListener('click', handleButtonClick);
-            globalSearchButton.classList.add('here');
-        }
-
-        return () => {
-            if (globalSearchInput) {
-                globalSearchInput.removeEventListener('click', handleInputClick);
-            }
-            if (globalSearchButton) {
-                globalSearchButton.removeEventListener('click', handleButtonClick);
-                globalSearchButton.classList.remove('here');
-            }
-        };
     }, []);
 
     return (
@@ -47,6 +22,7 @@ const GlobalSearch = () => {
                 isOpen={isModalOpen}
                 onClose={() => setIsModalOpen(false)}
             />}
+            <button onClick={handleButtonClick} class="btn btn-light"><i class="fa fa-search"></i>&nbsp;<span class="d-none d-md-inline">Search</span></button>
         </>
     );
 };
