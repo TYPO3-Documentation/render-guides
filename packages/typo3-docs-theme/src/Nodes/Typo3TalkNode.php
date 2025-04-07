4d4334e25cc67188f0bf4fe1238a09f35f0c7d94
lina.wolf <lwolf@w-commerce.de>
2025-04-07 13:35:45 +0200

    [FEATURE] Directive to connect to talk.typo3.org

--- packages/typo3-docs-theme/src/Nodes/Typo3TalkNode.php
+++ packages/typo3-docs-theme/src/Nodes/Typo3TalkNode.php
@@ -2,15 +2,11 @@
 
 namespace T3Docs\Typo3DocsTheme\Nodes;
 
-use phpDocumentor\Guides\Nodes\AbstractNode;
 use phpDocumentor\Guides\Nodes\Inline\PlainTextInlineNode;
 use phpDocumentor\Guides\Nodes\InlineCompoundNode;
 use phpDocumentor\Guides\Nodes\SectionNode;
 use phpDocumentor\Guides\RestructuredText\Nodes\GeneralDirectiveNode;
 
-/**
- * @extends AbstractNode<string>
- */
 final class Typo3TalkNode extends GeneralDirectiveNode
 {
     private ?SectionNode $sectionNode = null;
