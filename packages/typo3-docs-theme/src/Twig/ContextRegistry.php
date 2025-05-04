fcce0e8e5cf6df9755742e6abd4fc63c47ae0363
lina.wolf <lwolf@w-commerce.de>
2025-05-04 10:55:56 +0200

    [BUGFIX] Fix scrolling behavior for confvals
    And fix permalink display. Add tests for permalinks

--- packages/typo3-docs-theme/src/Twig/ContextRegistry.php
+++ packages/typo3-docs-theme/src/Twig/ContextRegistry.php
@@ -4,14 +4,17 @@ namespace T3Docs\Typo3DocsTheme\Twig;
 
 final class ContextRegistry
 {
+    /**
+     * @var array<string, string>
+     */
     private static array $store = [];
 
-    public static function set(string $key, mixed $value): void
+    public static function set(string $key, string $value): void
     {
         self::$store[$key] = $value;
     }
 
-    public static function get(string $key): mixed
+    public static function get(string $key): ?string
     {
         return self::$store[$key] ?? null;
     }
