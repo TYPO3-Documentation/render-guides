956c54dfd433d44cb9e7a602e05de2f6c14d8e8d
Sebastian Mendel <info@sebastianmendel.de>
2026-06-21 02:46:08 +0200

    [FEATURE] Support extension and local changelog links in :changelog:
    The :changelog: option now accepts, in addition to a bare core changelog
    entry identifier:
    
    * an explicit extension permalink shortcode, e.g.
      ":changelog: acme/acme-blog:changes-2-0-0" (vendor/package is normalized
      to the vendor-package form the permalink service expects), and
    * a short local form ":changelog: #changes-2-0-0" that links the current
      manual's own changelog, reusing its interlink-shortcode from guides.xml.
    
    This makes the standardized changelog link available to extension
    documentation, not only the TYPO3 core docs.

--- packages/typo3-docs-theme/src/Directives/Typo3VersionAddedDirective.php
+++ packages/typo3-docs-theme/src/Directives/Typo3VersionAddedDirective.php
@@ -6,12 +6,13 @@ namespace T3Docs\Typo3DocsTheme\Directives;
 
 use phpDocumentor\Guides\Nodes\CollectionNode;
 use phpDocumentor\Guides\RestructuredText\Parser\Productions\Rule;
+use T3Docs\Typo3DocsTheme\Settings\Typo3DocsThemeSettings;
 
 final class Typo3VersionAddedDirective extends AbstractTypo3VersionChangeDirective
 {
     /** @param Rule<CollectionNode> $startingRule */
-    public function __construct(Rule $startingRule)
+    public function __construct(Rule $startingRule, Typo3DocsThemeSettings $themeSettings)
     {
-        parent::__construct($startingRule, 'versionadded', 'New in version %s');
+        parent::__construct($startingRule, 'versionadded', 'New in version %s', $themeSettings);
     }
 }
